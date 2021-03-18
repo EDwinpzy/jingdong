e83ca2e992bc6fcc8c241d81a5cce54f734b5a06
EDwinpzy <630548043@qq.com>
2021-03-18 23:43:45 +0800

    3.18

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="docker">
+    <!-- BEM_CSS命名规则，__表示子元素，--表示状态 -->
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe616;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title">购物车</div>
+      </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe60c;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe607;</div>
+      <div class="docker__title">我的</div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.docker {
+  /* 将布局定义成容器 */
+  display: flex;
+  /* 盒模型，以容器的外边框作为长和宽（最外层） */
+  box-sizing: border-box;
+  /* 设置内边距(上右下左) */
+  position: absolute;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  /* 边框样式 */
+  border-top: 1px solid #F1F1F1;
+  &__item {
+    /* 自适应 */
+    flex: 1;
+    text-align: center;
+    /* docker__item下的iconfont样式 */
+    .iconfont {
+      /* 图标上下间距(上右下左) */
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
     }
+    &--active {
+      color: #1FA4FC;
+    }
+  }
+  &__title {
+    font-size: 20px;
+    /* 缩放 */
+    transform: scale(.5, .5);
+    /* 缩放中心点 */
+    transform-origin: center top;
   }
 }
 </style>
